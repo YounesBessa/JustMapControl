028709e19060af94196cec955bbe64bccb17f404
ClemBlue <clement.blin76@gmail.com>
2022-09-26 15:55:21 +0200

    add component resum match

---
+++ src/Components/ResumMatch/ResumMatch.jsx
@@ -0,0 +1,36 @@
+import './ResumMatch.css';
+
+const ResumMatch = ({}) => {
+
+    return(
+        <div>
+            <div class="flex container-vic">
+                <div class="flex dark-container">
+                    <p class="result-vic">Victoire</p>
+                    <div class="flex info">
+                        <p>Time</p>
+                        <p>KDA</p>
+                    </div>
+                </div>
+                <div class="flex image">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F52%2F59%2F23%2F525923ca003a0914085c833efc4d8901.jpg&f=1&nofb=1&ipt=8bd7652879e88fb80603b748b8386d1d2060b3bfe44d768ed3bc281e9d0637d4&ipo=images" />
+                    <p class="fly">LVL</p>
+                </div>
+            </div>
+            <div class="flex container">
+                <div class="flex dark-container">
+                    <p class="result">Défaite</p>
+                    <div class="flex info">
+                        <p>Time</p>
+                        <p>KDA</p>
+                    </div>
+                </div>
+                <div class="flex image">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F52%2F59%2F23%2F525923ca003a0914085c833efc4d8901.jpg&f=1&nofb=1&ipt=8bd7652879e88fb80603b748b8386d1d2060b3bfe44d768ed3bc281e9d0637d4&ipo=images" />
+                    <p class="fly">LVL</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ResumMatch;
