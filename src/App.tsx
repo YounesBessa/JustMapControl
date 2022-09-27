1e984a24ddecc76e9f14946d916f30da7b3de92b
ClemBlue <clement.blin76@gmail.com>
2022-09-27 11:25:07 +0200

    Add map and beacon components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/map';
+import Beacon from './components/beacon';
 
 function App() {
+
+  const x = '5420';
+  const y =  '12566';
+  const team = '200' ;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="mapContainer">
+          <Map />
+          <Beacon 
+          x={x}
+          y={y}
+          team={team}
+          />
+        </div>
+        
       </header>
     </div>
   );
---
+++ src/components/map/index.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+const Map = ({}) => {
+
+    return(
+        <div className="Map">
+        </div>
+    )
+}
+export default Map;
