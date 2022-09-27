3e5a7789cb5b8b27206bfe18f985d6eed79c7878
Younès Bessa <bessa460@gmail.com>
2022-09-27 12:18:52 +0200

    🎨 update searchbar component

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -2,26 +2,16 @@ import React from "react";
 import "./index.css";
 
 const Searchbar: React.FC = () => {
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-  };
 
   return (
-    <div className="SearchbarContainer"
-      onSubmit={handleSubmit}
-      >
-      <input className="FakeField"
-        type="text"
-        placeholder={"searchbar.where"}/>
-      <button className="SubmitButton" title={"Search"}>
-        <img className="SubmitIcon"
-          src={"/static/images/icons/search-plane.svg"}
-          alt={""}
-          width={50}
-          height={25}
-        />
-      </button>
-    </div>
+    <div className="SeachbarWrap">
+   <div className="Searchbar">
+      <input type="text" className="SearchTerm" placeholder="What are you looking for?"/>
+      <button type="submit" className="SearchButton">
+        <span className="ButtonText">Go</span>
+     </button>
+   </div>
+</div>
   );
 };
 
