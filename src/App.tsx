c6c8ea1853cc17132530edcbf8b49dd55b098345
Younès Bessa <bessa460@gmail.com>
2022-09-29 13:40:14 +0200

    🎨 update app tsx file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,29 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Searchbar from './components/Searchbar';
-import Footer from './components/Footer';
-import Map from './components/map';
-import Beacon from './components/beacon';
-import ScoreBoard from './components/scoreboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LayoutComponent from './components/Layout';
+import MatchPage from './pages/Match';
+import HomePage from './pages/Home';
+import TestPage from './pages/Test';
 
-function App() {
+export interface IApplicationProps {}
 
-  const x = 5420;
-  const y =  12566;
-  const team = '200' ;
+const Application: React.FunctionComponent<IApplicationProps> = (props) => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="match">
+                    <Route index element={<MatchPage />} />
+                    <Route path=":number" element={<MatchPage />} />
+                </Route>
+                <Route path="test" element={<TestPage />} />
+                <Route path="layout" element={<LayoutComponent />}>
+                    <Route index element={<MatchPage />} />
+                    <Route path=":number" element={<MatchPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-  const rKill = 12;
-  const bKill = 23;
-  const rGold = 35000;
-  const bGold = 49000;
-  const rNash = 0;
-  const bNash = 1;
-  const rDrag = 1;
-  const bDrag = 4;
-  const rTower = 3;
-  const bTower = 11;
-
-  return (
-    <div className="App">
-      <Header/>
-      <Searchbar/>
-        <div className="mapContainer">
-          <Map />
-          <Beacon 
-          x={x}
-          y={y}
-          team={team}
-          />
-        </div>
-        <div className="ScorBoard">
-          <ScoreBoard
-            rKill={rKill}
-            bKill={bKill}
-            rGold={rGold}
-            bGold={bGold}
-            rNash={rNash}
-            bNash={bNash}
-            rDrag={rDrag}
-            bDrag={bDrag}
-            rTower={rTower}
-            bTower={bTower}
-          />
-        </div>
-        <Footer/>
-        
-    </div>
-  );
-}
-
-export default App;
+export default Application;
