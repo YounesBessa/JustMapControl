825bb5b0007524cfd9f11ad88c51b015bdbb2835
Younès Bessa <bessa460@gmail.com>
2022-09-29 13:42:31 +0200

    🎨 update footer component structure

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './index.css';
 
 const Footer: React.FC = () => {
   return (
+    <Link to="/">
     <div className="FooterContainer">
       <div className="FooterContent">
         <p className="FooterText">© JustMapControl 2022 - All rights reserved</p>
         </div>
     </div>
+    </Link>
   );
 };
 
