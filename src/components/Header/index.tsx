e81a8c2034cf48c3d2acf5a1fc493634d3298ab9
Younès Bessa <bessa460@gmail.com>
2022-09-29 13:42:54 +0200

    🎨 update header component structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './index.css';
 
 const Header: React.FC = () => {
   return (
+    <Link to="/">
     <div className="HeaderContainer">
       <div className="ScuttlerContainer">
         <div className="Scuttler" />
         <div className="ScuttlerText">
         <h1 className="Title">JMC</h1>
         <h2 className="Subtitle">Just Map Control</h2>
+        <Link to="/match">Match</Link>
+        
         </div>
       </div>
     </div>
+    </Link>
   );
 };
 
