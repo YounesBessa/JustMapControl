3dcfde96c591929dc204387c1de45596bb0d9397
ClemBlue <clement.blin76@gmail.com>
2022-11-09 15:53:20 +0100

    Time line points sur la map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -8,6 +8,7 @@ const Map = () => {
     const { number } = useParams();
 
     const [matchtimeline, setmatch] = React.useState("vide");
+    const [info, setInfo] = React.useState('');
 
     const getmatch = (idmatch) => {
 
@@ -28,29 +29,67 @@ const Map = () => {
         getmatch(number);
     }, [number]);
 
-        if (matchtimeline !== "vide") {
-               var match = matchtimeline.matchJson.frames.map((frame) =>
-                    frame.events.map((event,index) => {
-                        if (event.type === "CHAMPION_KILL") {
-                            const x = event.position.x;
-                            const y = event.position.y;
-                            let team = "";
-                            if (event.killerId <= 5) {
-                                team = "100";
-                            } else {
-                                team = "200";
-                            }
-                            return <Beacon x={x} y={y} team={team} key={index} />;
+    var maxFrames = -1;
+   
+    function handleChange(event) {
+        const frames = event.target.value;
+        console.log(frames);
+        if(frames == maxFrames){
+            var match = matchtimeline.matchJson.frames.map((frame) =>
+                frame.events.map((event,index) => {
+                    if (event.type === "CHAMPION_KILL") {
+                        const x = event.position.x;
+                        const y = event.position.y;
+                        let team = "";
+                        if (event.killerId <= 5) {
+                            team = "100";
+                        } else {
+                            team = "200";
                         }
+                        return <Beacon x={x} y={y} team={team} key={index} />;
                     }
-                    )
+                }
                 )
+            );
+            setInfo(match);
+            
+        } else {
+            var match = matchtimeline.matchJson.frames[frames].events.map((event, index) =>{
+                if (event.type === "CHAMPION_KILL") {
+                    const x = event.position.x;
+                    const y = event.position.y;
+                    let team = "";
+                    if (event.killerId <= 5) {
+                        team = "100";
+                    } else {
+                        team = "200";
+                    }
+                    return <Beacon x={x} y={y} team={team} key={index} />;
+                }
+            });
+            console.log(match);
+            setInfo(match);
         }
+    }
+
+    if (matchtimeline !== "vide") {
+        
+    
+
+    matchtimeline.matchJson.frames.map((frame) => {
+        maxFrames++;
+    })
+    }
+        
     
 
     return(
-        <div className="Map">
-            {match}
+        <div className='contain'>
+            <div className="Map">
+                {info}
+            </div>
+            <label for="frames">Frames (0 et {maxFrames})</label>
+            <input type="range" onChange={handleChange} id="frames" name="frames" min="0" max={maxFrames}></input>
         </div>
     )
 }
