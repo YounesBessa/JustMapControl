649fe1329d9d0208d0943b9249b7578e90efe6b3
m02xime <maxkb02@gmail.com>
2022-11-30 15:05:32 +0100

    change acceuil

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import Header from '../components/Header';
-import Searchbar from '../components/Searchbar';
-import Footer from '../components/Footer';
-import Teemo from "../images/teemo.gif";
+import React from "react";
+import Header from "../components/Header";
+import Searchbar from "../components/Searchbar";
+import Footer from "../components/Footer";
 
 export interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
-    return (
-      <div>
-        <Header/>
-      <Searchbar/>
-      <p className="hello">{"Greetings, Summoner "}</p>
-      <img src={Teemo} className="teemo" alt="Teemo"/>
-      <p className="welcome">Welcome to JMC !</p>
-        <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Searchbar />
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
